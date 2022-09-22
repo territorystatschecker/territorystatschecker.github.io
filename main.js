dc7d589ce45253f56dffc42cd104c9d5d148e30b
territorystatschecker <113832329+territorystatschecker@users.noreply.github.com>
2022-09-22 13:09:47 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-var viewport = document.querySelector("meta[name=viewport]");
-viewport.setAttribute("content", viewport.content + ", height=" + window.innerHeight);
+var viewport = document.querySelector("meta[name=viewport]"); viewport.setAttribute("content", viewport.content + ", height=" + window.innerHeight);
 
 const hpm= [1,1.5,2,2.5,3.2,4,5,6.2,7.4,8.6,9.8,11];
 const dmgm= [1,1.4,1.8,2.2,2.6,3,3.4,3.8,4.2,4.6,5,5.4];
@@ -19,7 +18,14 @@ var lowdmg= parseInt(1000 * dmgm[Number(document.getElementById("damagen").value
 
 var highdmg= parseInt(1500 * dmgm[Number(document.getElementById("damagen").value)] * (1 + 0.3 * Number(document.getElementById("consn").value)));
 
+if((hp<300000000)&&(highdmg<500000))
+{
 document.getElementById("nresult").innerHTML="HQ Guild Tower Stats: <br><br>" + "<font color='#AA0000'>" + hp + " </font><font color='#AAAAAA'>(</font><font color='#FFAA00'>" + defv[Number(document.getElementById("defn").value)] + "</font><font color='#AAAAAA'>) - </font><font color='#FF5555'>" + lowdmg + "-" + highdmg + " <font color='#AAAAAA'>(</font><font color='#55FFFF'>" + asv[Number(document.getElementById("asn").value)] + "</font><font color='#AAAAAA'>)</font>";
+}
+else
+{
+document.getElementById("nresult").innerHTML="<font color='#FF5555'>Invalid Input!</font>";
+}
 }
 
 if(f==1)
@@ -32,7 +38,14 @@ var lowdmg= parseInt(1000 * dmgm[Number(document.getElementById("damagent").valu
 
 var highdmg= parseInt(1500 * dmgm[Number(document.getElementById("damagent").value)] * (1 + 0.3 * Number(document.getElementById("consn").value)));
 
+if((hp<300000000)&&(highdmg<500000))
+{
 document.getElementById("nresult").innerHTML="HQ Guild Tower Stats: <br><br>" + "<font color='#AA0000'>" + hp + " </font><font color='#AAAAAA'>(</font><font color='#FFAA00'>" + defv[Number(document.getElementById("defnt").value)] + "</font><font color='#AAAAAA'>) - </font><font color='#FF5555'>" + lowdmg + "-" + highdmg + " <font color='#AAAAAA'>(</font><font color='#55FFFF'>" + asv[Number(document.getElementById("asnt").value)] + "</font><font color='#AAAAAA'>)</font>";
+}
+else
+{
+document.getElementById("nresult").innerHTML="<font color='#FF5555'>Invalid Input!</font>";
+}
 }
 else
 {
@@ -53,7 +66,14 @@ var lowdmg= parseInt(1000 * (1.5 + 0.25 * (Number(document.getElementById("consh
 
 var highdmg= parseInt(1500 * (1.5 + 0.25 * (Number(document.getElementById("conshq").value) + Number(document.getElementById("ext").value))) * (1 + 0.3 * Number(document.getElementById("conshq").value)) * dmgm[Number(document.getElementById("damagehq").value)]);
 
+if((hp<300000000)&&(highdmg<500000))
+{
 document.getElementById("hqresult").innerHTML="HQ Guild Tower Stats: <br><br>" + "<font color='#AA0000'>" + hp + " </font><font color='#AAAAAA'>(</font><font color='#FFAA00'>" + defv[Number(document.getElementById("defhq").value)] + "</font><font color='#AAAAAA'>) - </font><font color='#FF5555'>" + lowdmg + "-" + highdmg + " <font color='#AAAAAA'>(</font><font color='#55FFFF'>" + asv[Number(document.getElementById("ashq").value)] + "</font><font color='#AAAAAA'>)</font>";
+}
+else
+{
+document.getElementById("hqresult").innerHTML="<font color='#FF5555'>Invalid Input!</font>";
+}
 }
 
 if(f==1)
@@ -66,7 +86,14 @@ var lowdmg= parseInt(1000 * (1.5 + 0.25 * (Number(document.getElementById("consh
 
 var highdmg= parseInt(1500 * (1.5 + 0.25 * (Number(document.getElementById("conshq").value) + Number(document.getElementById("ext").value))) * (1 + 0.3 * Number(document.getElementById("conshq").value)) * dmgm[Number(document.getElementById("damagehqt").value)]);
 
+if((hp<300000000)&&(highdmg<500000))
+{
 document.getElementById("hqresult").innerHTML="HQ Guild Tower Stats: <br><br>" + "<font color='#AA0000'>" + hp + " </font><font color='#AAAAAA'>(</font><font color='#FFAA00'>" + defv[Number(document.getElementById("defhqt").value)] + "</font><font color='#AAAAAA'>) - </font><font color='#FF5555'>" + lowdmg + "-" + highdmg + " <font color='#AAAAAA'>(</font><font color='#55FFFF'>" + asv[Number(document.getElementById("ashqt").value)] + "</font><font color='#AAAAAA'>)</font>";
+}
+else
+{
+document.getElementById("hqresult").innerHTML="<font color='#FF5555'>Invalid Input!</font>";
+}
 }
 else
 {
