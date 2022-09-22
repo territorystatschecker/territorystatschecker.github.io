65e42f2fc1ac97727f5ac1e9c207f468bce3c54d
territorystatschecker <113832329+territorystatschecker@users.noreply.github.com>
2022-09-22 18:14:03 +0530

    Update main.js

--- main.js
+++ main.js
@@ -12,15 +12,15 @@ function ncheck()
 
 if(f==0)
 {
-var hp= parseInt(300000 * hpm[Number(document.getElementById("hpn").value)] * (1 + 0.3 * Number(document.getElementById("consn").value)));
+var hp= 300000 * hpm[Number(document.getElementById("hpn").value)] * (1 + 0.3 * Number(document.getElementById("consn").value));
 
-var lowdmg= parseInt(1000 * dmgm[Number(document.getElementById("damagen").value)] * (1 + 0.3 * Number(document.getElementById("consn").value)));
+var lowdmg= 1000 * dmgm[Number(document.getElementById("damagen").value)] * (1 + 0.3 * Number(document.getElementById("consn").value));
 
-var highdmg= parseInt(1500 * dmgm[Number(document.getElementById("damagen").value)] * (1 + 0.3 * Number(document.getElementById("consn").value)));
+var highdmg= 1500 * dmgm[Number(document.getElementById("damagen").value)] * (1 + 0.3 * Number(document.getElementById("consn").value));
 
 if((hp<300000000)&&(highdmg<500000))
 {
-document.getElementById("nresult").innerHTML="HQ Guild Tower Stats: <br><br>" + "<font color='#AA0000'>" + hp + " </font><font color='#AAAAAA'>(</font><font color='#FFAA00'>" + defv[Number(document.getElementById("defn").value)] + "</font><font color='#AAAAAA'>) - </font><font color='#FF5555'>" + lowdmg + "-" + highdmg + " <font color='#AAAAAA'>(</font><font color='#55FFFF'>" + asv[Number(document.getElementById("asn").value)] + "</font><font color='#AAAAAA'>)</font>";
+document.getElementById("nresult").innerHTML="HQ Guild Tower Stats: <br><br>" + "<font color='#AA0000'>" + parseInt(hp) + " </font><font color='#AAAAAA'>(</font><font color='#FFAA00'>" + defv[Number(document.getElementById("defn").value)] + "</font><font color='#AAAAAA'>) - </font><font color='#FF5555'>" + parseInt(lowdmg) + "-" + parseInt(highdmg) + " <font color='#AAAAAA'>(</font><font color='#55FFFF'>" + asv[Number(document.getElementById("asn").value)] + "</font><font color='#AAAAAA'>)</font>";
 }
 else
 {
@@ -32,15 +32,15 @@ if(f==1)
 {
 if((document.getElementById("damagent").value>=0)&&(document.getElementById("damagent").value<12)&&(document.getElementById("asnt").value>=0)&&(document.getElementById("asnt").value<12)&&(document.getElementById("hpnt").value>=0)&&(document.getElementById("hpnt").value<12)&&(document.getElementById("defnt").value>=0)&&(document.getElementById("defnt").value<12))
 {
-var hp= parseInt(300000 * hpm[Number(document.getElementById("hpnt").value)] * (1 + 0.3 * Number(document.getElementById("consn").value)));
+var hp= 300000 * hpm[Number(document.getElementById("hpnt").value)] * (1 + 0.3 * Number(document.getElementById("consn").value));
 
-var lowdmg= parseInt(1000 * dmgm[Number(document.getElementById("damagent").value)] * (1 + 0.3 * Number(document.getElementById("consn").value)));
+var lowdmg= 1000 * dmgm[Number(document.getElementById("damagent").value)] * (1 + 0.3 * Number(document.getElementById("consn").value));
 
-var highdmg= parseInt(1500 * dmgm[Number(document.getElementById("damagent").value)] * (1 + 0.3 * Number(document.getElementById("consn").value)));
+var highdmg= 1500 * dmgm[Number(document.getElementById("damagent").value)] * (1 + 0.3 * Number(document.getElementById("consn").value));
 
 if((hp<300000000)&&(highdmg<500000))
 {
-document.getElementById("nresult").innerHTML="HQ Guild Tower Stats: <br><br>" + "<font color='#AA0000'>" + hp + " </font><font color='#AAAAAA'>(</font><font color='#FFAA00'>" + defv[Number(document.getElementById("defnt").value)] + "</font><font color='#AAAAAA'>) - </font><font color='#FF5555'>" + lowdmg + "-" + highdmg + " <font color='#AAAAAA'>(</font><font color='#55FFFF'>" + asv[Number(document.getElementById("asnt").value)] + "</font><font color='#AAAAAA'>)</font>";
+document.getElementById("nresult").innerHTML="HQ Guild Tower Stats: <br><br>" + "<font color='#AA0000'>" + parseInt(hp) + " </font><font color='#AAAAAA'>(</font><font color='#FFAA00'>" + defv[Number(document.getElementById("defnt").value)] + "</font><font color='#AAAAAA'>) - </font><font color='#FF5555'>" + parseInt(lowdmg) + "-" + parseInt(highdmg) + " <font color='#AAAAAA'>(</font><font color='#55FFFF'>" + asv[Number(document.getElementById("asnt").value)] + "</font><font color='#AAAAAA'>)</font>";
 }
 else
 {
@@ -60,15 +60,15 @@ function hqcheck()
 
 if(f==0)
 {
-var hp= parseInt(300000 * (1.5 + 0.25 * (Number(document.getElementById("conshq").value) + Number(document.getElementById("ext").value))) * (1 + 0.3 * Number(document.getElementById("conshq").value)) * hpm[Number(document.getElementById("hphq").value)]);
+var hp= 300000 * (1.5 + 0.25 * (Number(document.getElementById("conshq").value) + Number(document.getElementById("ext").value))) * (1 + 0.3 * Number(document.getElementById("conshq").value)) * hpm[Number(document.getElementById("hphq").value)];
 
-var lowdmg= parseInt(1000 * (1.5 + 0.25 * (Number(document.getElementById("conshq").value) + Number(document.getElementById("ext").value))) * (1 + 0.3 * Number(document.getElementById("conshq").value)) * dmgm[Number(document.getElementById("damagehq").value)]);
+var lowdmg= 1000 * (1.5 + 0.25 * (Number(document.getElementById("conshq").value) + Number(document.getElementById("ext").value))) * (1 + 0.3 * Number(document.getElementById("conshq").value)) * dmgm[Number(document.getElementById("damagehq").value)];
 
-var highdmg= parseInt(1500 * (1.5 + 0.25 * (Number(document.getElementById("conshq").value) + Number(document.getElementById("ext").value))) * (1 + 0.3 * Number(document.getElementById("conshq").value)) * dmgm[Number(document.getElementById("damagehq").value)]);
+var highdmg= 1500 * (1.5 + 0.25 * (Number(document.getElementById("conshq").value) + Number(document.getElementById("ext").value))) * (1 + 0.3 * Number(document.getElementById("conshq").value)) * dmgm[Number(document.getElementById("damagehq").value)];
 
 if((hp<300000000)&&(highdmg<500000))
 {
-document.getElementById("hqresult").innerHTML="HQ Guild Tower Stats: <br><br>" + "<font color='#AA0000'>" + hp + " </font><font color='#AAAAAA'>(</font><font color='#FFAA00'>" + defv[Number(document.getElementById("defhq").value)] + "</font><font color='#AAAAAA'>) - </font><font color='#FF5555'>" + lowdmg + "-" + highdmg + " <font color='#AAAAAA'>(</font><font color='#55FFFF'>" + asv[Number(document.getElementById("ashq").value)] + "</font><font color='#AAAAAA'>)</font>";
+document.getElementById("hqresult").innerHTML="HQ Guild Tower Stats: <br><br>" + "<font color='#AA0000'>" + parseInt(hp) + " </font><font color='#AAAAAA'>(</font><font color='#FFAA00'>" + defv[Number(document.getElementById("defhq").value)] + "</font><font color='#AAAAAA'>) - </font><font color='#FF5555'>" + parseInt(lowdmg) + "-" + parseInt(highdmg) + " <font color='#AAAAAA'>(</font><font color='#55FFFF'>" + asv[Number(document.getElementById("ashq").value)] + "</font><font color='#AAAAAA'>)</font>";
 }
 else
 {
@@ -80,15 +80,15 @@ if(f==1)
 {
 if((document.getElementById("damagehqt").value>=0)&&(document.getElementById("damagehqt").value<12)&&(document.getElementById("ashqt").value>=0)&&(document.getElementById("ashqt").value<12)&&(document.getElementById("hphqt").value>=0)&&(document.getElementById("hphqt").value<12)&&(document.getElementById("defhqt").value>=0)&&(document.getElementById("defhqt").value<12))
 {
-var hp= parseInt(300000 * (1.5 + 0.25 * (Number(document.getElementById("conshq").value) + Number(document.getElementById("ext").value))) * (1 + 0.3 * Number(document.getElementById("conshq").value)) * hpm[Number(document.getElementById("hphqt").value)]);
+var hp= 300000 * (1.5 + 0.25 * (Number(document.getElementById("conshq").value) + Number(document.getElementById("ext").value))) * (1 + 0.3 * Number(document.getElementById("conshq").value)) * hpm[Number(document.getElementById("hphqt").value)];
 
-var lowdmg= parseInt(1000 * (1.5 + 0.25 * (Number(document.getElementById("conshq").value) + Number(document.getElementById("ext").value))) * (1 + 0.3 * Number(document.getElementById("conshq").value)) * dmgm[Number(document.getElementById("damagehqt").value)]);
+var lowdmg= 1000 * (1.5 + 0.25 * (Number(document.getElementById("conshq").value) + Number(document.getElementById("ext").value))) * (1 + 0.3 * Number(document.getElementById("conshq").value)) * dmgm[Number(document.getElementById("damagehqt").value)];
 
-var highdmg= parseInt(1500 * (1.5 + 0.25 * (Number(document.getElementById("conshq").value) + Number(document.getElementById("ext").value))) * (1 + 0.3 * Number(document.getElementById("conshq").value)) * dmgm[Number(document.getElementById("damagehqt").value)]);
+var highdmg= 1500 * (1.5 + 0.25 * (Number(document.getElementById("conshq").value) + Number(document.getElementById("ext").value))) * (1 + 0.3 * Number(document.getElementById("conshq").value)) * dmgm[Number(document.getElementById("damagehqt").value)];
 
 if((hp<300000000)&&(highdmg<500000))
 {
-document.getElementById("hqresult").innerHTML="HQ Guild Tower Stats: <br><br>" + "<font color='#AA0000'>" + hp + " </font><font color='#AAAAAA'>(</font><font color='#FFAA00'>" + defv[Number(document.getElementById("defhqt").value)] + "</font><font color='#AAAAAA'>) - </font><font color='#FF5555'>" + lowdmg + "-" + highdmg + " <font color='#AAAAAA'>(</font><font color='#55FFFF'>" + asv[Number(document.getElementById("ashqt").value)] + "</font><font color='#AAAAAA'>)</font>";
+document.getElementById("hqresult").innerHTML="HQ Guild Tower Stats: <br><br>" + "<font color='#AA0000'>" + parseInt(hp) + " </font><font color='#AAAAAA'>(</font><font color='#FFAA00'>" + defv[Number(document.getElementById("defhqt").value)] + "</font><font color='#AAAAAA'>) - </font><font color='#FF5555'>" + parseInt(lowdmg) + "-" + parseInt(highdmg) + " <font color='#AAAAAA'>(</font><font color='#55FFFF'>" + asv[Number(document.getElementById("ashqt").value)] + "</font><font color='#AAAAAA'>)</font>";
 }
 else
 {
